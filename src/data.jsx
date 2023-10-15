46fbada67b9b18fe053dc79a698bed421a3e247b
JittapolSrisawang <jittapol.s1986@gmail.com>
2023-10-15 21:48:40 +0700

    Update URL

--- src/data.jsx
+++ src/data.jsx
@@ -5,7 +5,7 @@ export const projects = [
     id: nanoid(),
     img: "images/portfolio.jpg",
     url: "https://jittapol-srisawang-portfolio.netlify.app",
-    github: "https://github.com/JittapolSrisawang",
+    github: "https://github.com/JittapolSrisawang/web-dev-portfolio",
     title: "Web Developer Portfolio",
     text: "A website for presenting information and portfolios of web developers.",
     tools: "React + TailwindCSS",
